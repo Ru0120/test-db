e4f359be2f31df482b494f27edc973216cd678c2
Ru0120 <bavuumsk16@gmail.com>
2025-02-06 20:40:48 +0800

    mongoose

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import { Player } from "./db/schemas/Player.js";
 
 dotenv.config();
 
@@ -12,6 +13,29 @@ mongoose.connect(url).then(() => {
 });
 
 const app = express();
+
+app.post("/", async (req, res) => {
+  try {
+    await Player.create({
+      firstName: "Dorj",
+      lastName: "dorj",
+      age: 17,
+      height: 180,
+      weight: 80,
+      history: [{ team: "lakers", awards: "mvp" }],
+      historyObject: { team: "lakers", awards: "mvp" },
+    });
+    res.send("success");
+  } catch (e) {
+    res.send(`error: ${e.message}`);
+  }
+});
+
+app.get("/", async (req, res) => {
+  const player = await Player.find({ lastName: "dorj" });
+
+  res.send(player);
+});
 
 app.listen(port, () => {
   console.log(`app running on ${port}`);
